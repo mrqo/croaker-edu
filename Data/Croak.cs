04f6a86e032d032b6bf8269f5620cadf487e311c
elMarco <elmarcopl@gmail.com>
2019-10-03 16:09:56 +0200

    Hashtag extraction from Croak.

--- Data/Croak.cs
+++ Data/Croak.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace edu_croaker.Data
 {
@@ -10,8 +12,32 @@ namespace edu_croaker.Data
 
         public string Author { get; set; }
 
-        public string Content { get; set; }
+        private string _content;
+        public string Content 
+        { 
+            get => _content;
+            set
+            {
+                _content = value;
+                ExtractHashtags();
+            } 
+        }
 
         public int Shares { get; set; }
+
+        private List<string> _hashtags = new List<string>();
+        public ReadOnlyCollection<string> Hashtags => new ReadOnlyCollection<string>(_hashtags);
+
+        protected void ExtractHashtags()
+        {
+            var tokens = Content.Split(' ');
+            foreach (var token in tokens)
+            {
+                if (token.StartsWith('#'))
+                {
+                    _hashtags.Add(token.TrimStart('#'));
+                }
+            }
+        }
     }
 }
