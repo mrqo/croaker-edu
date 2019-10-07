772f43f9a659b29d21a8878a1be57bc98b710f5c
elMarco <elmarcopl@gmail.com>
2019-10-07 09:43:35 +0200

    Reworked dialog for writing messages.

--- Data/Croak.cs
+++ Data/Croak.cs
@@ -12,32 +12,10 @@ namespace edu_croaker.Data
 
         public string Author { get; set; }
 
-        private string _content;
-        public string Content 
-        { 
-            get => _content;
-            set
-            {
-                _content = value;
-                ExtractHashtags();
-            } 
-        }
+        public string Content { get; set; }
 
         public int Shares { get; set; }
 
-        private List<string> _hashtags = new List<string>();
-        public ReadOnlyCollection<string> Hashtags => new ReadOnlyCollection<string>(_hashtags);
-
-        protected void ExtractHashtags()
-        {
-            var tokens = Content.Split(' ');
-            foreach (var token in tokens)
-            {
-                if (token.StartsWith('#'))
-                {
-                    _hashtags.Add(token.TrimStart('#'));
-                }
-            }
-        }
+        public List<string> Hashtags { get; set; } = new List<string>();
     }
 }
