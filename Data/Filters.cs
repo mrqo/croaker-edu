a22ef51ec1709d1a798cc82314ed54086cea417a
elMarco <elmarcopl@gmail.com>
2019-10-15 10:05:41 +0200

    Extracted components from index.

---
+++ Data/Filters.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace edu_croaker.Data
+{
+    public enum CroakFetchFilter
+    {
+        ByUser,
+        ByHashtag,
+        Generic
+    }
+}
